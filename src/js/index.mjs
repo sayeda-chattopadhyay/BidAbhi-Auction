e70293257f583fad509be5d76483580ac94e52a5
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-02-05 23:55:01 +0100

    creates router

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,3 +1,18 @@
-import { setRegisterFormListener } from "./handlers/signUp.mjs";
+import * as handler from "./handlers/index.mjs";
 
-setRegisterFormListener();
+function router() {
+  const path = location.pathname;
+  console.log(path);
+
+  switch (path) {
+    case "/signUp.html":
+      handler.setRegisterFormListener();
+      break;
+
+    case "/login.html":
+      handler.setLoginFormListener();
+      break;
+  }
+}
+
+router();
