77a53fff84155bc414ce09edffb56af154e21afe
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-02-07 19:49:02 +0100

    adds logout func into the home

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -2,6 +2,8 @@ import * as handler from "./handlers/index.mjs";
 
 import * as ui from "./ui/index.mjs";
 
+// import * as api from "./api/profiles/index.mjs";
+
 // import * as listings from "./api/listings/index.mjs";
 
 function router() {
@@ -9,22 +11,26 @@ function router() {
   console.log(path);
 
   switch (path) {
+    case "/login.html":
+      handler.setLoginFormListener();
+      break;
+
     case "/signUp.html":
       handler.setRegisterFormListener();
 
       break;
 
-    case "/login.html":
-      handler.setLoginFormListener();
-      break;
-
     case "/profile.html":
       handler.setLogoutBUttonListener();
+      // ui.displaySingleProfile();
+      // api.getProfileDetails();
+
       break;
 
     case "/index.html":
       // listings.getAllListings();
       ui.displayAllListings();
+      handler.setLogoutBUttonListener();
 
       break;
   }
