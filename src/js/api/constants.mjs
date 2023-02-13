160331d16d442342700a3af2feac502ed6e8387a
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-02-13 20:47:43 +0100

    removes console.log

--- src/js/api/constants.mjs
+++ src/js/api/constants.mjs
@@ -16,18 +16,23 @@ export const loginURL = `${baseURL}${authLogin}`;
 
 export const profile = "/profiles";
 export const profileURL = `${baseURL}${profile}${"/"}`;
-console.log(profileURL);
+// console.log(profileURL);
 
 // Auction All listings endpoints
 
 export const listings = "/listings";
 export const listingsBaseURL = `${baseURL}${listings}`;
-console.log(listingsBaseURL);
+// console.log(listingsBaseURL);
 
 // Auction Active listings endpoints
 
 export const activeListing = "?_active=true";
 export const activeListingURL = `${listingsBaseURL}${activeListing}`;
+
+// filter active listings by tag using the _tag query flag.
+
+export const searchListingQs = "&_tag=";
+export const searchListingUrl = `${activeListingURL}${searchListingQs}`;
 
 // The listings can be sorted with the sort flags:
 
@@ -35,4 +40,4 @@ export const listingSorts = "?sort=created&sortOrder=desc";
 export const listingSortsURL = `${listingsBaseURL}${activeListing}`;
 
 export const singlelistingUrl = `${listingsBaseURL}${"/"}`;
-console.log(singlelistingUrl);
+// console.log(singlelistingUrl);
