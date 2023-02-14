06e22d093c58222a433a70d4c0948dcb7e6666b6
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-02-14 20:29:34 +0100

    fetches all listings created by profile

--- src/js/api/profiles/getAllListingsByProfile.mjs
+++ src/js/api/profiles/getAllListingsByProfile.mjs
@@ -1,6 +1,7 @@
 import { profileURL } from "../constants.mjs";
 
 import { load } from "../../storage/index.mjs";
+import { createAllProfileListingsHTML } from "../../templates/index.mjs";
 
 console.log(profileURL);
 
@@ -38,59 +39,3 @@ export async function getAllListingsByProfile() {
 
 getAllListingsByProfile();
 
-// innerHtml
-const allListingByProfileContainer = document.getElementById(
-  "allListingByProfileContainer"
-);
-
-export function createAllProfileListingsHTML(listings) {
-  //   if (listings.length === 0) {
-  //     return listingsContainer.innerHTML === "No item found";
-  //   }
-
-  allListingByProfileContainer.innerHTML = "";
-
-  listings.forEach(function (listing) {
-    const { id, title, description, media, created, endsAt, _count, tags } =
-      listing;
-
-    allListingByProfileContainer.innerHTML += `<div class="col p-3">
-                                                    <div class="card h-100 border-0 p-2">
-                                                        <a href=""
-                                                            ><img
-                                                            src="${media}"
-                                                            class="card-img-top"
-                                                            style="height: 22rem; object-fit: cover"
-                                                            alt="${title}"
-                                                        /></a>
-                                                        <div class="card-body bg-light">
-                                                            <h3 class="card-title pb-0">${title}</h3>
-                                                            <p class="card-text">
-                                                            <strong>Description: </strong>${description}
-                                                            </p>
-                                                            <p class="card-text">
-                                                            <strong>Tags: </strong>${tags}
-                                                            </p>
-                                                            <p class="card-text"><strong>Owner: </strong>Owner name</p>
-                                                        </div>
-                                                        <div class="card-footer bg-light border-0 pt-0 mt-0">
-                                                            <hr />
-                                                            <p class="mb-0"><strong>Bids: </strong>${_count.bids}</p>
-                                                            <p class="mb-0">
-                                                            <strong>Created: </strong>${created}<span class="small text-muted"
-                                                                >, 16:20</span
-                                                            >
-                                                            </p>
-                                                            <p class="mb-3">
-                                                            <strong>Ends at: </strong>${endsAt}<span class="small text-muted"
-                                                                >, 17:35</span
-                                                            >
-                                                            </p>
-                                                            <a href="/singleListing.html?id=${id}"class="btn btn-primary" style="width: 50%">
-                                                                        <strong>View Item</strong>
-                                                                    </a>
-                                                        </div>
-                                                    </div>
-                                                  </div>`;
-  });
-}
