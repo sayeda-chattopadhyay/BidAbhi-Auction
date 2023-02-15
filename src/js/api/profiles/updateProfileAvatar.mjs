724f6049c7b49d2ccb133d60af661823cf59a6b3
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-02-15 12:21:16 +0100

    creates update profile avatar

---
+++ src/js/api/profiles/updateProfileAvatar.mjs
@@ -0,0 +1,41 @@
+import { profileURL } from "../constants.mjs";
+import { load } from "../../storage/index.mjs";
+
+const token = load("token");
+console.log(token);
+
+const profile = load("profile");
+console.log(profile);
+
+const profileName = profile.name;
+console.log(profileName);
+
+const getProfileUrl = `${profileURL}${profileName}`;
+console.log(getProfileUrl);
+
+const qs = "/media";
+
+const method = "put";
+
+export async function updateProfileAvatar(profileAvatar) {
+  try {
+    const profileAvatarUpdateUrl = `${getProfileUrl}${qs}`;
+    console.log(profileAvatarUpdateUrl);
+
+    const response = await fetch(profileAvatarUpdateUrl, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      method: method,
+      body: JSON.stringify(profileAvatar),
+    });
+    // if (response.ok) {
+    //   return await response.json();
+    // }
+    const Updatedprofile = await response.json();
+    console.log(Updatedprofile);
+  } catch (error) {
+    console.log(error);
+  }
+}
---
+++ src/js/handlers/updateProfileAvatar.mjs
@@ -0,0 +1,25 @@
+import { updateProfileAvatar } from "../api/profiles/updateProfileAvatar.mjs";
+
+export async function setUpdateProfileAvatar() {
+  const form = document.querySelector("form#updateProfileAvatar");
+  const input = document.querySelector("#floatingInput");
+  console.log(input);
+  console.log(form);
+  if (form) {
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const form = event.target;
+      const formData = new FormData(form);
+      const profileAvatar = Object.fromEntries(formData.entries());
+      console.log(profileAvatar);
+
+      try {
+        updateProfileAvatar(profileAvatar);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  }
+}
+
+setUpdateProfileAvatar();
