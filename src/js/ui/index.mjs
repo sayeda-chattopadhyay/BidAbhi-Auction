941d1cb5f059e67442bbd711333996a406f11072
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-02-24 00:11:26 +0100

    adds error message

--- src/js/ui/index.mjs
+++ src/js/ui/index.mjs
@@ -1,4 +1,7 @@
 // export * from "./displayAllListings.mjs";
-export * from "./displaySearchedListings.mjs";
+// export * from "../templates/listings/displaySearchedListings.mjs";
 // export * from "./displaySingleListing.mjs";
 // export * from "./displaySingleProfile.mjs";
+
+export * from "./displayLoader.mjs";
+export * from "./displayErrorMessage.mjs";
