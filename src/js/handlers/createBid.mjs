c6043c4bdefa917bb815621564c06a935d597a51
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-02-24 20:33:37 +0100

    hides bidHistory section

--- src/js/handlers/createBid.mjs
+++ src/js/handlers/createBid.mjs
@@ -8,7 +8,7 @@ export async function setCreateBidListener() {
   const button = document.querySelector("#bid-button");
   const bidMessage = document.querySelector("#bid-message");
   const noCreditMessage = document.querySelector("#noCredit-message")
-  const bidHistory = document.getElementById("bidHistory");
+  const bidHistorySection = document.getElementById("bid-history-section");
   noCreditMessage.style.display ="none";
   const token = load("token")
   
@@ -16,7 +16,7 @@ export async function setCreateBidListener() {
   if (!token){
     button.disabled = true;
     bidMessage.style.display="block";
-    bidHistory.style.display="";
+    bidHistorySection.style.display="none";
   }
   else {
     bidMessage.style.display="none";
@@ -36,60 +36,3 @@ export async function setCreateBidListener() {
   }
 }
 
-// setCreateBidListener();
-
-// please log in to place you bid
-
-//  if (!loggedIn) && (form) {
-//   button.disabled = true;
-//  }else{
-//
-//   form.addEventListener("submit", async (event) => {
-//     event.preventDefault();
-//     const form = event.target;
-//     const formData = new FormData(form);
-//     const amount = formData.get("amount");
-//     const bidAmount = Number(amount);
-
-//     console.log(bidAmount);
-
-//     try {
-//       createBidOnlisting(bidAmount);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   });
-//
-
-// }
-
-
-
-
-// actual function
-
-
-// export async function setCreateBidListener() {
-//   const form = document.querySelector("form#bid-form");
-//   const button = document.querySelector("#bid-button");
-//   const token = load("token")
-//   console.log(token)
-//   button.disabled = true;
-//   console.log(button);
-
-//   if (form) {
-//     form.addEventListener("submit", async (event) => {
-//       event.preventDefault();
-//       const form = event.target;
-//       const formData = new FormData(form);
-//       const amount = formData.get("amount");
-//       const bidAmount = Number(amount);
-
-//       try {
-//         createBidOnlisting(bidAmount);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     });
-//   }
-// }
