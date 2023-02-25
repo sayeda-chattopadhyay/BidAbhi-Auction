358e93bef2234159bfe647a527356607eedad37c
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-02-25 01:50:47 +0100

    deletes console log

--- src/js/api/profiles/getSingleProfile.mjs
+++ src/js/api/profiles/getSingleProfile.mjs
@@ -3,23 +3,23 @@ import { load } from "../../storage/index.mjs";
 import { createSingleProfileHTML } from "../../templates/index.mjs";
 
 const token = load("token");
-console.log(token);
+
 
 const profile = load("profile");
-console.log(profile);
+
 
 const profileName = profile.name;
-console.log(profileName);
+
 
 const getProfileUrl = `${profileURL}${profileName}`;
-console.log(getProfileUrl);
+
 
 const qs = "?_listings=true";
 
 export async function getProfileDetails() {
   try {
     const bidListinsUrl = `${getProfileUrl}${qs}`;
-    console.log(bidListinsUrl);
+    
 
     const response = await fetch(bidListinsUrl, {
       headers: {
