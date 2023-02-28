9a001d01e388da5b0461689dee6ae50d30c35298
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-02-28 17:44:32 +0100

    cleans file

--- src/js/api/listings/index.mjs
+++ src/js/api/listings/index.mjs
@@ -1,10 +1,10 @@
 export * from "./createBidOnListing.mjs";
 export * from "./createListing.mjs";
 export * from "./getAllListings.mjs";
-// export * from "./getSingleListing.mjs";
 export * from "./searchListing.mjs";
 export * from "./editListing.mjs";
 export * from "./getActiveListings.mjs";
+export * from "./deleteListing.mjs";
 
 // single listing
 
