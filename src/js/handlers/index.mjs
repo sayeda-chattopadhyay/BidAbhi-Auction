6b5c67ea4965575a7da1c44bbfe9882f75b049de
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-02-28 17:46:03 +0100

    cleans file

--- src/js/handlers/index.mjs
+++ src/js/handlers/index.mjs
@@ -1,11 +1,10 @@
 export * from "./signUp.mjs";
 export * from "./logIn.mjs";
 export * from "./logout.mjs";
-// export * from "./getAllListings.mjs";
 export * from "./createListing.mjs";
 export * from "./searchListing.mjs";
 export * from "./createBid.mjs";
 export * from "./header.mjs";
-
 export * from "./editListing.mjs";
+export * from "./deleteListing.mjs";
 // export * from "./updateProfileAvatar.mjs";
