bc642ac1785d5ef4aa21b5ac1184c49167236d17
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-03-01 19:57:34 +0100

    if block doesn´t seem working

--- src/js/handlers/createBid.mjs
+++ src/js/handlers/createBid.mjs
@@ -8,9 +8,11 @@ import {load} from "../storage/index.mjs"
 
 export async function setCreateBidListener() {
   const form = document.querySelector("form#bid-form");
-  const bidButton = document.querySelector("#bid-button");
+  console.log("Bid form", form)
 
+  const bidButton = document.querySelector("#bid-button");
   console.log("bidButton:", bidButton)
+
   const bidMessage = document.querySelector("#bid-message");
   //const bidSectionContainer = document.querySelector("#bid-section");
   const noCreditMessage = document.querySelector("#noCredit-message")
@@ -23,8 +25,10 @@ export async function setCreateBidListener() {
 
   const token = load("token")
   console.log("token:", token)
+
   const profile = load("profile")
   console.log("localStorageProfile:",profile )
+
   const profileName = profile.name;
   console.log("localStorageProfileName:",profileName )
 
@@ -33,20 +37,11 @@ export async function setCreateBidListener() {
   // const listingId = urlParams.get("id");
   // console.log("listingId:",listingId );
  
-  
- 
 
   if (!token){
-    bidButton.style.display = true;
-    bidMessage.style.display="block";
-    
-    // bidHistorySection.style.display="none";
-    // bidSection.style.display= "none"
+    form.style.display="none"
   }
-  // if(profileName === sellerName){
-  //   placeYourBidSection.style.display= "none"
-
-  // }
+ 
   else {
     bidMessage.style.display="none";
     form.addEventListener("submit", async (event) => {
@@ -68,3 +63,12 @@ export async function setCreateBidListener() {
 
 
 
+  // bidButton.style.display = true;
+    // bidMessage.style.display="block";
+    
+    // bidHistorySection.style.display="none";
+    // bidSection.style.display= "none"
+  // if(profileName === sellerName){
+  //   placeYourBidSection.style.display= "none"
+
+  // }
