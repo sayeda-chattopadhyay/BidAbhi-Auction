137442208aed3aa9ee82dc6828768acf39bf3fa2
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-03-01 19:59:40 +0100

    problem in if block

--- src/js/handlers/createBid.mjs
+++ src/js/handlers/createBid.mjs
@@ -4,8 +4,6 @@ import {load} from "../storage/index.mjs"
 
  //import {createSingleListingHTML} from "../renderHtml/Listing/singleListing.mjs"
 
-
-
 export async function setCreateBidListener() {
   const form = document.querySelector("form#bid-form");
   console.log("Bid form", form)
@@ -38,26 +36,50 @@ export async function setCreateBidListener() {
   // console.log("listingId:",listingId );
  
 
-  if (!token){
-    form.style.display="none"
-  }
- 
-  else {
-    bidMessage.style.display="none";
+  // if (!token){
+  //   console.log("No token")
+  //   bidButton.disabled = true;
+  //   // form.style.display="none"
+  //   bidMessage.style.display="block";
+  // }else {
+  //   bidMessage.style.display="none";
+  //   form.addEventListener("submit", async (event) => {
+  //     event.preventDefault();
+  //     const form = event.target;
+  //     const formData = new FormData(form);
+  //     const amount = formData.get("amount");
+  //     const bidAmount = Number(amount);  
+  //     try {
+  //       createBidOnlisting(bidAmount);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   });
+  // }
+
+if(!token){
+  // form.style.display="none"
+  bidButton.disabled = true;
+  bidMessage.style.display="block";
+  console.log(" There is no token")
+}else{
+   bidMessage.style.display="none";
+   console.log(" There is token")
+
     form.addEventListener("submit", async (event) => {
       event.preventDefault();
       const form = event.target;
       const formData = new FormData(form);
       const amount = formData.get("amount");
-      const bidAmount = Number(amount);
-
+      const bidAmount = Number(amount);  
       try {
         createBidOnlisting(bidAmount);
       } catch (error) {
         console.log(error);
       }
     });
-  }
+}
+
 }
 
 
