7db0e7db2a7b8ec172da385e56e1fe19d1d37a86
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-03-04 15:11:33 +0100

    adds bootstrap loader

--- src/js/ui/displayLoader.mjs
+++ src/js/ui/displayLoader.mjs
@@ -3,12 +3,33 @@
  * @param {number} containerId this is the id of the html conatiner.
  */
 
+// export function displayLoading(conatinerId) {
+//   const container = document.getElementById(conatinerId);
+//   container.innerHTML = "";
+//   const div = document.createElement("div");
+//     // const loadingIndicator = document.createTextNode("Loading...");
+//     // div.appendChild(loadingIndicator);
+//   div.classList.add("loader");
+//   // div.classList.add("spinner-border");
+
+//   container.appendChild(div);
+// }
+
+
+// customised 
+
 export function displayLoading(conatinerId) {
   const container = document.getElementById(conatinerId);
   container.innerHTML = "";
+  const spinnerWrapper =document.createElement("div");
+  spinnerWrapper.classList.add("text-center")
+
   const div = document.createElement("div");
-  //   const loadingIndicator = document.createTextNode("Loading...");
-  //   div.appendChild(loadingIndicator);
-  div.classList.add("loader");
-  container.appendChild(div);
+  div.classList.add("spinner-border");
+  const span =  document.createElement("span"); 
+  span.classList.add("visually-hidden");
+   
+  div.appendChild(span);
+  spinnerWrapper.appendChild(div)
+  container.appendChild(spinnerWrapper);
 }
