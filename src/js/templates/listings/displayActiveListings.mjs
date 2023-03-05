3a06dd38e93fcd8fd724a8dd44c360b17f3122e8
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-03-05 21:32:53 +0100

    changes to lower case

--- src/js/templates/listings/displayActiveListings.mjs
+++ src/js/templates/listings/displayActiveListings.mjs
@@ -1,12 +1,12 @@
 import * as api from "../../api/listings/index.mjs";
-import * as renderHTml from "../../renderHtml/Listing/index.mjs";
+import * as renderhtml from "../../renderhtml/listing/index.mjs";
 import * as ui from "../../ui/index.mjs";
 
 export async function displayActiveListings() {
   try {
     // ui.displayLoading("activeListingsContainer"); //  Loading indication to the html listing container
     const listings = await api.getActiveListings() // getting all listings from api
-    renderHTml.createActiveListingsHtml(listings); // passing these listings to HTML page
+    renderhtml.createActiveListingsHtml(listings); // passing these listings to HTML page
   } catch (error) {
     console.log(error);
     ui.displayError("activeListingsContainer", error); // displays error message
