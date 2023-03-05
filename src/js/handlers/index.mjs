f05f50851c27377f190f2f402006341294b5b149
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-03-05 22:41:25 +0100

    cleans router

--- src/js/handlers/index.mjs
+++ src/js/handlers/index.mjs
@@ -1,4 +1,4 @@
-export * from "./signup.mjs";
+
 export * from "./login.mjs";
 export * from "./logout.mjs";
 export * from "./createlisting.mjs";
@@ -6,6 +6,8 @@ export * from "./searchlisting.mjs";
 export * from "./createbid.mjs";
 export * from "./header.mjs";
 
+
+// export * from "./signup.mjs";
 //export * from "./editListing.mjs";
 //export * from "./deleteListing.mjs";
 // export * from "./updateProfileAvatar.mjs";
