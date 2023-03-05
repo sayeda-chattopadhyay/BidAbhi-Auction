d005577493db63689458cc447368b8bca156a1d8
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-03-05 09:41:35 +0100

    comments out now, will work on this function later

--- src/js/templates/profile/getSingleListingByProfile.mjs
+++ src/js/templates/profile/getSingleListingByProfile.mjs
@@ -1,31 +1,31 @@
 
 
-  export function renderBidButtonOrEditButtons(username, bidName,id) {
+  // export function renderBidButtonOrEditButtons(username, bidName,id) {
 
-  //    const placeYourBidSection = document.querySelector("#place-your-bid-section")
+  // //    const placeYourBidSection = document.querySelector("#place-your-bid-section")
 
-  //    console.log("placeYourBidSection:", placeYourBidSection)
+  // //    console.log("placeYourBidSection:", placeYourBidSection)
 
-     if(!username) {
+  //    if(!username) {
 
-    return "<div>You must be loggd in to bid</div>"
+  //   return "<div>You must be loggd in to bid</div>"
    
-   } 
+  //  } 
 
-  console.log("user name:", username )
-  console.log("bidder name:", bidName )
+  // console.log("user name:", username )
+  // console.log("bidder name:", bidName )
     
-    if(username === bidName) {
+  //   if(username === bidName) {
      
-      return `
-      <a href="/edit-item.html?id=${id}" class="btn btn-primary bg-success">Edit Post</a> 
-      <button data-delete-id=${id} class="delete-post border border-0 bg-info">Delete Post</button>`
+  //     return `
+  //     <a href="/edit-item.html?id=${id}" class="btn btn-primary bg-success">Edit Post</a> 
+  //     <button data-delete-id=${id} class="delete-post border border-0 bg-info">Delete Post</button>`
 
-    }
+  //   }
   
-     return "<div></div>"; 
+  //    return "<div></div>"; 
 
-  }
+  // }
 
 
   
